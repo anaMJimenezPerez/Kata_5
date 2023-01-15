a849bf1f8c9b4ae5543a7e593088f72a8bbbb92e
anam0 <anam0@LAPTOP-VDUU5KM1>
2023-01-15 20:03:22 +0000

    Kata 5 version 2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,11 +1,14 @@
 
 package kata5;
 
+import static kata5.CrearTabla.createNewTable;
+
 public class Kata5 {
 
     public static void main(String[] args) {
         SelectApp app = new SelectApp();
-        app.selectAll();
+        //app.selectAll();
+        createNewTable();
     }
 
     
